53531915f417440f09c593a1c8dc3e394abd6d09
Chenx Dust <chenx_dust@outlook.com>
2026-07-03 00:16:03 +0800

    Support iOS
    Fix wifi_ssid plugin for iOS
    
    UI support iOS
    
    Fix macOS localization and copyright
    
    Add build tool
    
    Unify bundle id
    
    Fix waitForTunnelConnected
    
    Update Makefile for iOS core build

--- core/lib.go
+++ core/lib.go
@@ -13,19 +13,19 @@ import (
 	t "core/tun"
 	"encoding/json"
 	"errors"
-	"github.com/metacubex/mihomo/component/dialer"
-	"github.com/metacubex/mihomo/component/process"
-	"github.com/metacubex/mihomo/constant"
-	"github.com/metacubex/mihomo/dns"
-	"github.com/metacubex/mihomo/listener/sing_tun"
-	"github.com/metacubex/mihomo/log"
-	"golang.org/x/sync/semaphore"
 	"net"
-	"strings"
 	"sync"
 	"syscall"
 	"time"
 	"unsafe"
+
+	"github.com/metacubex/mihomo/common/lowmemory"
+	"github.com/metacubex/mihomo/component/dialer"
+	"github.com/metacubex/mihomo/component/process"
+	"github.com/metacubex/mihomo/constant"
+	"github.com/metacubex/mihomo/listener/sing_tun"
+	"github.com/metacubex/mihomo/log"
+	"golang.org/x/sync/semaphore"
 )
 
 const stopTrace = "[STOP-TRACE]"
@@ -205,14 +205,12 @@ func handleStartTun(callback unsafe.Pointer, fd int, stack, address, dns string)
 		}
 		tunHandler.start(fd, stack, address, dns)
 	}
+	return
 }
 
-func handleUpdateDns(value string) {
-	go func() {
-		log.Infoln("[DNS] updateDns %s", value)
-		dns.UpdateSystemDNS(strings.Split(value, ","))
-		dns.FlushCacheWithDefaultResolver()
-	}()
+//export setLowMemoryMode
+func setLowMemoryMode(enabled bool) {
+	lowmemory.SetEnabled(enabled)
 }
 
 func (response MethodResponse) send() {

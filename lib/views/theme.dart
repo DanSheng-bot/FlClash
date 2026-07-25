chenx-dust/FlClash-Patched

Support Predictive Back

lib/views/theme.dart
@@ -6,6 +6,7 @@ import 'dart:ui' as ui;
 import 'package:fl_clash/common/common.dart';
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/models/models.dart';
+import 'package:fl_clash/providers/app.dart';
 import 'package:fl_clash/providers/config.dart';
 import 'package:fl_clash/state.dart';
 import 'package:fl_clash/widgets/widgets.dart';
@@ -47,6 +48,7 @@ class ThemeView extends StatelessWidget {
           _PrimaryColorItem(),
           SliverToBoxAdapter(child: SizedBox(height: 16)),
           _PrueBlackItem(),
+          _PredictiveBackItem(),
           SliverToBoxAdapter(child: SizedBox(height: 16)),
           _TextScaleFactorItem(),
           SliverToBoxAdapter(child: SizedBox(height: 32)),
@@ -287,6 +289,7 @@ class _PrimaryColorItemState extends ConsumerState<_PrimaryColorItem> {
 
     return SliverToBoxAdapter(
       child: CommonPopScope(
+        canPop: _removablePrimaryColor == null,
         onPop: (context) {
           if (_removablePrimaryColor != null) {
             setState(() {
@@ -443,6 +446,44 @@ class _PrueBlackItem extends ConsumerWidget {
             ref
                 .read(themeSettingProvider.notifier)
                 .update((state) => state.copyWith(pureBlack: value));
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class _PredictiveBackItem extends ConsumerWidget {
+  const _PredictiveBackItem();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final supportsPredictiveBack = system.supportsPredictiveBack(
+      ref.watch(versionProvider),
+    );
+    if (!supportsPredictiveBack) {
+      return const SliverToBoxAdapter();
+    }
+    final appLocalizations = context.appLocalizations;
+    final predictiveBack = ref.watch(
+      themeSettingProvider.select((state) => state.predictiveBack),
+    );
+    return SliverToBoxAdapter(
+      child: ListItem.switchItem(
+        leading: const Icon(Icons.swipe_right_alt),
+        horizontalTitleGap: 12,
+        title: Text(
+          appLocalizations.predictiveBack,
+          style: Theme.of(context).textTheme.titleSmall?.copyWith(
+            color: context.colorScheme.onSurfaceVariant,
+          ),
+        ),
+        delegate: SwitchDelegate(
+          value: predictiveBack,
+          onChanged: (value) {
+            ref
+                .read(themeSettingProvider.notifier)
+                .update((state) => state.copyWith(predictiveBack: value));
           },
         ),
       ),

DanSheng-bot/FlClash

lib/views/theme.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 import 'package:fl_clash/common/common.dart';
 import 'package:fl_clash/enum/enum.dart';
 import 'package:fl_clash/models/models.dart';
+import 'package:fl_clash/providers/app.dart';
 import 'package:fl_clash/providers/config.dart';
 import 'package:fl_clash/state.dart';
 import 'package:fl_clash/widgets/widgets.dart';
@@ -47,6 +48,7 @@ class ThemeView extends StatelessWidget {
           _PrimaryColorItem(),
           SliverToBoxAdapter(child: SizedBox(height: 16)),
           _PrueBlackItem(),
+          _PredictiveBackItem(),
           SliverToBoxAdapter(child: SizedBox(height: 16)),
           _TextScaleFactorItem(),
           SliverToBoxAdapter(child: SizedBox(height: 32)),
@@ -287,6 +289,7 @@ class _PrimaryColorItemState extends ConsumerState<_PrimaryColorItem> {
 
     return SliverToBoxAdapter(
       child: CommonPopScope(
+        canPop: _removablePrimaryColor == null,
         onPop: (context) {
           if (_removablePrimaryColor != null) {
             setState(() {
@@ -442,6 +445,42 @@ class _PrueBlackItem extends ConsumerWidget {
           ref
               .read(themeSettingProvider.notifier)
               .update((state) => state.copyWith(pureBlack: value));
+        },
+      ),
+    );
+  }
+}
+
+class _PredictiveBackItem extends ConsumerWidget {
+  const _PredictiveBackItem();
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final supportsPredictiveBack = system.supportsPredictiveBack(
+      ref.watch(versionProvider),
+    );
+    if (!supportsPredictiveBack) {
+      return const SliverToBoxAdapter();
+    }
+    final appLocalizations = context.appLocalizations;
+    final predictiveBack = ref.watch(
+      themeSettingProvider.select((state) => state.predictiveBack),
+    );
+    return SliverToBoxAdapter(
+      child: ListItem.toggle(
+        leading: const Icon(Icons.swipe_right_alt),
+        horizontalTitleGap: 12,
+        title: Text(
+          appLocalizations.predictiveBack,
+          style: Theme.of(context).textTheme.titleSmall?.copyWith(
+            color: context.colorScheme.onSurfaceVariant,
+          ),
+        ),
+        value: predictiveBack,
+        onChanged: (value) {
+          ref
+              .read(themeSettingProvider.notifier)
+              .update((state) => state.copyWith(predictiveBack: value));
         },
       ),
     );
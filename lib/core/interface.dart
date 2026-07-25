chenx-dust/FlClash-Patched

Decrypt age subscription

lib/core/interface.dart
@@ -18,6 +18,8 @@ mixin CoreInterface {
 
   Future<String> validateConfig(String path, {String? ageSecretKey});
 
+  Future<String> decryptAgeConfig(String data, String ageSecretKey);
+
   Future<Result> getConfig(String path, {String? ageSecretKey});
 
   Future<Map<String, String>> generateAgeKeyPair();
@@ -158,6 +160,19 @@ abstract class CoreHandlerInterface with CoreInterface {
     };
     return await _invoke<String>(
           method: ActionMethod.validateConfig,
+          data: json.encode(params),
+        ) ??
+        '';
+  }
+
+  @override
+  Future<String> decryptAgeConfig(String data, String ageSecretKey) async {
+    final params = {
+      'data': data,
+      'age-secret-key': ageSecretKey,
+    };
+    return await _invoke<String>(
+          method: ActionMethod.decryptAgeConfig,
           data: json.encode(params),
         ) ??
         '';

DanSheng-bot/FlClash

lib/core/interface.dart
@@ -19,6 +19,8 @@ mixin CoreInterface {
 
   Future<String> validateConfig(String path);
 
+  Future<String> decryptAgeConfig(String data, String ageSecretKey);
+
   Future<Map<String, dynamic>> getConfig(String path);
 
   Future<Map<String, String>> generateAgeKeyPair();
@@ -160,6 +162,15 @@ abstract class CoreHandlerInterface with CoreInterface {
     return await _invokeMethod<String>(
           method: CoreMethod.validateConfig,
           arguments: path,
+        ) ??
+        '';
+  }
+
+  @override
+  Future<String> decryptAgeConfig(String data, String ageSecretKey) async {
+    return await _invokeMethod<String>(
+          method: CoreMethod.decryptAgeConfig,
+          arguments: {'data': data, 'age-secret-key': ageSecretKey},
         ) ??
         '';
   }
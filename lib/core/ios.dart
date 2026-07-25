chenx-dust/FlClash-Patched

Remove unnecessary age methods

lib/core/ios.dart
@@ -18,10 +18,12 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
     ActionMethod.getIsInit,
     ActionMethod.validateConfig,
     ActionMethod.getConfig,
+    ActionMethod.decryptAgeConfig,
     ActionMethod.generateAgeKeyPair,
     ActionMethod.convertAgeSecretKeyToPublicKey,
     ActionMethod.deleteFile,
     ActionMethod.updateGeoData,
+    ActionMethod.getCountryCode,
   };
 
   Completer<bool> _connectedCompleter = Completer();

DanSheng-bot/FlClash

lib/core/ios.dart
@@ -18,10 +18,12 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
     CoreMethod.getIsInit,
     CoreMethod.validateConfig,
     CoreMethod.getConfig,
+    CoreMethod.decryptAgeConfig,
     CoreMethod.generateAgeKeyPair,
     CoreMethod.convertAgeSecretKeyToPublicKey,
     CoreMethod.deleteFile,
     CoreMethod.updateGeoData,
+    CoreMethod.getCountryCode,
   };
 
   Completer<bool> _connectedCompleter = Completer();
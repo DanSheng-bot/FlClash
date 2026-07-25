chenx-dust/FlClash-Patched

Fix action method routing

lib/core/ios.dart
@@ -15,11 +15,11 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
 
   static const _appCoreMethods = {
     ActionMethod.validateConfig,
-    ActionMethod.asyncTestDelay,
     ActionMethod.getConfig,
     ActionMethod.generateAgeKeyPair,
     ActionMethod.convertAgeSecretKeyToPublicKey,
     ActionMethod.deleteFile,
+    ActionMethod.updateGeoData,
   };
 
   Completer<bool> _connectedCompleter = Completer();
@@ -136,6 +136,12 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
     final stopped = await service?.stop() ?? false;
     _isNetworkExtensionCoreActive = false;
     commonPrint.log('[iOS] stop NECore result: $stopped');
+    if (_setupParams != null) {
+      await invoke<String>(
+        method: ActionMethod.setupConfig,
+        data: json.encode(_setupParams),
+      );
+    }
     return stopped;
   }
 

DanSheng-bot/FlClash

lib/core/ios.dart
@@ -15,11 +15,11 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
 
   static const _appCoreMethods = {
     CoreMethod.validateConfig,
-    CoreMethod.asyncTestDelay,
     CoreMethod.getConfig,
     CoreMethod.generateAgeKeyPair,
     CoreMethod.convertAgeSecretKeyToPublicKey,
     CoreMethod.deleteFile,
+    CoreMethod.updateGeoData,
   };
 
   Completer<bool> _connectedCompleter = Completer();
@@ -127,6 +127,12 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
     final stopped = await service?.stop() ?? false;
     _isNetworkExtensionCoreActive = false;
     commonPrint.log('[iOS] stop NECore result: $stopped');
+    if (_setupParams != null) {
+      await invokeMethod<String>(
+        method: CoreMethod.setupConfig,
+        arguments: _setupParams!.toJson(),
+      );
+    }
     return stopped;
   }
 
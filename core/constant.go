chenx-dust/FlClash-Patched

Send request message only if request panel is showing

core/constant.go
@@ -109,6 +109,8 @@ const (
 	sideLoadExternalProviderMethod       Method = "sideLoadExternalProvider"
 	startLogMethod                       Method = "startLog"
 	stopLogMethod                        Method = "stopLog"
+	startRequestMethod                   Method = "startRequest"
+	stopRequestMethod                    Method = "stopRequest"
 	startListenerMethod                  Method = "startListener"
 	stopListenerMethod                   Method = "stopListener"
 	updateDnsMethod                      Method = "updateDns"

lib/core/interface.dart
@@ -65,6 +65,10 @@ mixin CoreInterface {
 
   FutureOr<void> stopLog();
 
+  FutureOr<void> startRequest();
+
+  FutureOr<void> stopRequest();
+
   Future<bool> crash();
 
   FutureOr<String> getConnections();
@@ -344,6 +348,16 @@ abstract class CoreHandlerInterface with CoreInterface {
   @override
   FutureOr<void> stopLog() {
     _invoke<bool>(method: ActionMethod.stopLog);
+  }
+
+  @override
+  FutureOr<void> startRequest() {
+    _invoke(method: ActionMethod.startRequest);
+  }
+
+  @override
+  FutureOr<void> stopRequest() {
+    _invoke<bool>(method: ActionMethod.stopRequest);
   }
 
   @override

DanSheng-bot/FlClash

core/constant.go
@@ -106,6 +106,8 @@ const (
 	sideLoadExternalProviderMethod       CoreMethod = "sideLoadExternalProvider"
 	startLogMethod                       CoreMethod = "startLog"
 	stopLogMethod                        CoreMethod = "stopLog"
+	startRequestMethod                   CoreMethod = "startRequest"
+	stopRequestMethod                    CoreMethod = "stopRequest"
 	startListenerMethod                  CoreMethod = "startListener"
 	stopListenerMethod                   CoreMethod = "stopListener"
 	updateDnsMethod                      CoreMethod = "updateDns"

lib/core/interface.dart
@@ -66,6 +66,10 @@ mixin CoreInterface {
 
   FutureOr<void> stopLog();
 
+  FutureOr<void> startRequest();
+
+  FutureOr<void> stopRequest();
+
   Future<bool> crash();
 
   FutureOr<List<TrackerInfo>> getConnections();
@@ -366,6 +370,16 @@ abstract class CoreHandlerInterface with CoreInterface {
   @override
   FutureOr<void> stopLog() {
     _invokeMethod<bool>(method: CoreMethod.stopLog);
+  }
+
+  @override
+  FutureOr<void> startRequest() {
+    _invokeMethod(method: CoreMethod.startRequest);
+  }
+
+  @override
+  FutureOr<void> stopRequest() {
+    _invokeMethod<bool>(method: CoreMethod.stopRequest);
   }
 
   @override
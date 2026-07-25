chenx-dust/FlClash-Patched

Mixed port can be zero to disable

lib/l10n/intl/messages_en.dart
@@ -60,7 +60,8 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m15(label) => "${label} must be a number";
 
-  static String m16(label) => "${label} must be between 1024 and 49151";
+  static String m16(label) =>
+      "${label} must be between 1024 and 49151, 0 to disable";
 
   static String m17(count) => "${count} items have been selected";
 

lib/l10n/intl/messages_ja.dart
@@ -53,7 +53,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m15(label) => "${label} は数字でなければなりません";
 
-  static String m16(label) => "${label} は 1024 から 49151 の間でなければなりません";
+  static String m16(label) => "${label} は 1024 から 49151 の間でなければなりません，0 は無効です";
 
   static String m17(count) => "${count} 項目が選択されています";
 

lib/l10n/intl/messages_ru.dart
@@ -59,7 +59,8 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m15(label) => "${label} должно быть числом";
 
-  static String m16(label) => "${label} должен быть числом от 1024 до 49151";
+  static String m16(label) =>
+      "${label} должен быть числом от 1024 до 49151, 0 для отключения";
 
   static String m17(count) => "Выбрано ${count} элементов";
 

lib/l10n/intl/messages_zh_CN.dart
@@ -53,7 +53,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m15(label) => "${label} 必须为数字";
 
-  static String m16(label) => "${label} 必须在 1024 到 49151 之间";
+  static String m16(label) => "${label} 必须在 1024 到 49151 之间，0 为禁用";
 
   static String m17(count) => "已选择 ${count} 项";
 
@@ -531,7 +531,7 @@ class MessageLookup extends MessageLookupByLibrary {
     "resourcesDesc": MessageLookupByLibrary.simpleMessage("外部资源相关信息"),
     "respectRules": MessageLookupByLibrary.simpleMessage("遵守规则"),
     "respectRulesDesc": MessageLookupByLibrary.simpleMessage(
-      "DNS 连接跟随 rules, 需配置 proxy-server-nameserver",
+      "DNS 连接跟随 rules，需配置 proxy-server-nameserver",
     ),
     "restart": MessageLookupByLibrary.simpleMessage("重启"),
     "restartCoreTip": MessageLookupByLibrary.simpleMessage("您确定要重启核心吗？"),
@@ -581,7 +581,7 @@ class MessageLookup extends MessageLookupByLibrary {
     ),
     "ruleActionIpAsnDesc": MessageLookupByLibrary.simpleMessage("匹配 IP 所属 ASN"),
     "ruleActionIpCidr6Desc": MessageLookupByLibrary.simpleMessage(
-      "匹配 IP 地址范围, IP-CIDR6 只是一个别名",
+      "匹配 IP 地址范围，IP-CIDR6 只是一个别名",
     ),
     "ruleActionIpCidrDesc": MessageLookupByLibrary.simpleMessage("匹配 IP 地址范围"),
     "ruleActionIpSuffixDesc": MessageLookupByLibrary.simpleMessage(

DanSheng-bot/FlClash

lib/l10n/intl/messages_en.dart
@@ -70,7 +70,8 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m20(label) => "${label} must be a number";
 
-  static String m21(label) => "${label} must be between 1024 and 49151";
+  static String m21(label) =>
+      "${label} must be between 1024 and 49151, 0 to disable";
 
   static String m22(count) => "${count} seconds";
 

lib/l10n/intl/messages_ja.dart
@@ -63,7 +63,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m20(label) => "${label} は数字でなければなりません";
 
-  static String m21(label) => "${label} は 1024 から 49151 の間でなければなりません";
+  static String m21(label) => "${label} は 1024 から 49151 の間でなければなりません，0 は無効です";
 
   static String m22(count) => "${count} 秒";
 

lib/l10n/intl/messages_ru.dart
@@ -69,7 +69,8 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m20(label) => "${label} должно быть числом";
 
-  static String m21(label) => "${label} должен быть числом от 1024 до 49151";
+  static String m21(label) =>
+      "${label} должен быть числом от 1024 до 49151, 0 для отключения";
 
   static String m22(count) => "${count} секунд";
 

lib/l10n/intl/messages_zh_CN.dart
@@ -63,7 +63,7 @@ class MessageLookup extends MessageLookupByLibrary {
 
   static String m20(label) => "${label} 必须为数字";
 
-  static String m21(label) => "${label} 必须在 1024 到 49151 之间";
+  static String m21(label) => "${label} 必须在 1024 到 49151 之间，0 为禁用";
 
   static String m22(count) => "${count} 秒";
 
@@ -560,7 +560,7 @@ class MessageLookup extends MessageLookupByLibrary {
     "resourcesDesc": MessageLookupByLibrary.simpleMessage("外部资源相关信息"),
     "respectRules": MessageLookupByLibrary.simpleMessage("遵守规则"),
     "respectRulesDesc": MessageLookupByLibrary.simpleMessage(
-      "DNS 连接跟随 rules, 需配置 proxy-server-nameserver",
+      "DNS 连接跟随 rules，需配置 proxy-server-nameserver",
     ),
     "restart": MessageLookupByLibrary.simpleMessage("重启"),
     "restartCoreTip": MessageLookupByLibrary.simpleMessage("您确定要重启核心吗？"),
@@ -610,7 +610,7 @@ class MessageLookup extends MessageLookupByLibrary {
     ),
     "ruleActionIpAsnDesc": MessageLookupByLibrary.simpleMessage("匹配 IP 所属 ASN"),
     "ruleActionIpCidr6Desc": MessageLookupByLibrary.simpleMessage(
-      "匹配 IP 地址范围, IP-CIDR6 只是一个别名",
+      "匹配 IP 地址范围，IP-CIDR6 只是一个别名",
     ),
     "ruleActionIpCidrDesc": MessageLookupByLibrary.simpleMessage("匹配 IP 地址范围"),
     "ruleActionIpSuffixDesc": MessageLookupByLibrary.simpleMessage(
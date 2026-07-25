chenx-dust/FlClash-Patched

Store recent requests when notify not enabled

core/constant.go
@@ -109,8 +109,8 @@ const (
 	sideLoadExternalProviderMethod       Method = "sideLoadExternalProvider"
 	startLogMethod                       Method = "startLog"
 	stopLogMethod                        Method = "stopLog"
-	startRequestMethod                   Method = "startRequest"
-	stopRequestMethod                    Method = "stopRequest"
+	startRequestNotifyMethod             Method = "startRequestNotify"
+	stopRequestNotifyMethod              Method = "stopRequestNotify"
 	startListenerMethod                  Method = "startListener"
 	stopListenerMethod                   Method = "stopListener"
 	updateDnsMethod                      Method = "updateDns"

lib/core/interface.dart
@@ -65,9 +65,9 @@ mixin CoreInterface {
 
   FutureOr<void> stopLog();
 
-  FutureOr<void> startRequest();
+  FutureOr<List<TrackerInfo>> startRequestNotify();
 
-  FutureOr<void> stopRequest();
+  FutureOr<void> stopRequestNotify();
 
   Future<bool> crash();
 
@@ -351,13 +351,22 @@ abstract class CoreHandlerInterface with CoreInterface {
   }
 
   @override
-  FutureOr<void> startRequest() {
-    _invoke(method: ActionMethod.startRequest);
+  Future<List<TrackerInfo>> startRequestNotify() async {
+    final res = await _invoke<List<dynamic>>(
+      method: ActionMethod.startRequestNotify,
+    );
+    if (res == null || res.isEmpty) {
+      return [];
+    }
+    return res
+        .whereType<Map>()
+        .map((item) => TrackerInfo.fromJson(Map<String, Object?>.from(item)))
+        .toList();
   }
 
   @override
-  FutureOr<void> stopRequest() {
-    _invoke<bool>(method: ActionMethod.stopRequest);
+  FutureOr<void> stopRequestNotify() {
+    _invoke<bool>(method: ActionMethod.stopRequestNotify);
   }
 
   @override

DanSheng-bot/FlClash

core/constant.go
@@ -106,8 +106,8 @@ const (
 	sideLoadExternalProviderMethod       CoreMethod = "sideLoadExternalProvider"
 	startLogMethod                       CoreMethod = "startLog"
 	stopLogMethod                        CoreMethod = "stopLog"
-	startRequestMethod                   CoreMethod = "startRequest"
-	stopRequestMethod                    CoreMethod = "stopRequest"
+	startRequestNotifyMethod             CoreMethod = "startRequestNotify"
+	stopRequestNotifyMethod              CoreMethod = "stopRequestNotify"
 	startListenerMethod                  CoreMethod = "startListener"
 	stopListenerMethod                   CoreMethod = "stopListener"
 	updateDnsMethod                      CoreMethod = "updateDns"

lib/core/interface.dart
@@ -66,9 +66,9 @@ mixin CoreInterface {
 
   FutureOr<void> stopLog();
 
-  FutureOr<void> startRequest();
+  FutureOr<List<TrackerInfo>> startRequestNotify();
 
-  FutureOr<void> stopRequest();
+  FutureOr<void> stopRequestNotify();
 
   Future<bool> crash();
 
@@ -373,13 +373,22 @@ abstract class CoreHandlerInterface with CoreInterface {
   }
 
   @override
-  FutureOr<void> startRequest() {
-    _invokeMethod(method: CoreMethod.startRequest);
+  Future<List<TrackerInfo>> startRequestNotify() async {
+    final res = await _invokeMethod<List<dynamic>>(
+      method: CoreMethod.startRequestNotify,
+    );
+    if (res == null || res.isEmpty) {
+      return [];
+    }
+    return res
+        .whereType<Map>()
+        .map((item) => TrackerInfo.fromJson(Map<String, Object?>.from(item)))
+        .toList();
   }
 
   @override
-  FutureOr<void> stopRequest() {
-    _invokeMethod<bool>(method: CoreMethod.stopRequest);
+  FutureOr<void> stopRequestNotify() {
+    _invokeMethod<bool>(method: CoreMethod.stopRequestNotify);
   }
 
   @override
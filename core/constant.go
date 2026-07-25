chenx-dust/FlClash-Patched

Support age key

core/constant.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"encoding/json"
+	"net/netip"
+	"time"
+
 	"github.com/metacubex/mihomo/adapter/provider"
 	P "github.com/metacubex/mihomo/component/process"
 	"github.com/metacubex/mihomo/constant"
 	"github.com/metacubex/mihomo/log"
 	"github.com/metacubex/mihomo/tunnel"
-	"net/netip"
-	"time"
 )
+
+type ValidateConfigParams struct {
+	Path         string `json:"path"`
+	AgeSecretKey string `json:"age-secret-key"`
+}
+
+type GetConfigParams struct {
+	Path         string `json:"path"`
+	AgeSecretKey string `json:"age-secret-key"`
+}
 
 type InitParams struct {
 	HomeDir string `json:"home-dir"`
@@ -72,39 +83,41 @@ type ProxiesData struct {
 }
 
 const (
-	messageMethod                  Method = "message"
-	initClashMethod                Method = "initClash"
-	getIsInitMethod                Method = "getIsInit"
-	forceGcMethod                  Method = "forceGc"
-	shutdownMethod                 Method = "shutdown"
-	validateConfigMethod           Method = "validateConfig"
-	updateConfigMethod             Method = "updateConfig"
-	getProxiesMethod               Method = "getProxies"
-	changeProxyMethod              Method = "changeProxy"
-	getTrafficMethod               Method = "getTraffic"
-	getTotalTrafficMethod          Method = "getTotalTraffic"
-	resetTrafficMethod             Method = "resetTraffic"
-	asyncTestDelayMethod           Method = "asyncTestDelay"
-	getConnectionsMethod           Method = "getConnections"
-	closeConnectionsMethod         Method = "closeConnections"
-	resetConnectionsMethod         Method = "resetConnections"
-	closeConnectionMethod          Method = "closeConnection"
-	getExternalProvidersMethod     Method = "getExternalProviders"
-	getExternalProviderMethod      Method = "getExternalProvider"
-	getCountryCodeMethod           Method = "getCountryCode"
-	getMemoryMethod                Method = "getMemory"
-	updateGeoDataMethod            Method = "updateGeoData"
-	updateExternalProviderMethod   Method = "updateExternalProvider"
-	sideLoadExternalProviderMethod Method = "sideLoadExternalProvider"
-	startLogMethod                 Method = "startLog"
-	stopLogMethod                  Method = "stopLog"
-	startListenerMethod            Method = "startListener"
-	stopListenerMethod             Method = "stopListener"
-	updateDnsMethod                Method = "updateDns"
-	crashMethod                    Method = "crash"
-	setupConfigMethod              Method = "setupConfig"
-	getConfigMethod                Method = "getConfig"
-	deleteFile                     Method = "deleteFile"
+	messageMethod                        Method = "message"
+	initClashMethod                      Method = "initClash"
+	getIsInitMethod                      Method = "getIsInit"
+	forceGcMethod                        Method = "forceGc"
+	shutdownMethod                       Method = "shutdown"
+	validateConfigMethod                 Method = "validateConfig"
+	updateConfigMethod                   Method = "updateConfig"
+	getProxiesMethod                     Method = "getProxies"
+	changeProxyMethod                    Method = "changeProxy"
+	getTrafficMethod                     Method = "getTraffic"
+	getTotalTrafficMethod                Method = "getTotalTraffic"
+	resetTrafficMethod                   Method = "resetTraffic"
+	asyncTestDelayMethod                 Method = "asyncTestDelay"
+	getConnectionsMethod                 Method = "getConnections"
+	closeConnectionsMethod               Method = "closeConnections"
+	resetConnectionsMethod               Method = "resetConnections"
+	closeConnectionMethod                Method = "closeConnection"
+	getExternalProvidersMethod           Method = "getExternalProviders"
+	getExternalProviderMethod            Method = "getExternalProvider"
+	getCountryCodeMethod                 Method = "getCountryCode"
+	getMemoryMethod                      Method = "getMemory"
+	updateGeoDataMethod                  Method = "updateGeoData"
+	updateExternalProviderMethod         Method = "updateExternalProvider"
+	sideLoadExternalProviderMethod       Method = "sideLoadExternalProvider"
+	startLogMethod                       Method = "startLog"
+	stopLogMethod                        Method = "stopLog"
+	startListenerMethod                  Method = "startListener"
+	stopListenerMethod                   Method = "stopListener"
+	updateDnsMethod                      Method = "updateDns"
+	crashMethod                          Method = "crash"
+	setupConfigMethod                    Method = "setupConfig"
+	getConfigMethod                      Method = "getConfig"
+	deleteFile                           Method = "deleteFile"
+	generateAgeKeyPairMethod             Method = "generateAgeKeyPair"
+	convertAgeSecretKeyToPublicKeyMethod Method = "convertAgeSecretKeyToPublicKey"
 )
 
 type Method string

DanSheng-bot/FlClash

core/constant.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"net/netip"
+	"time"
+
 	"github.com/metacubex/mihomo/adapter/provider"
 	P "github.com/metacubex/mihomo/component/process"
 	"github.com/metacubex/mihomo/constant"
 	"github.com/metacubex/mihomo/log"
 	"github.com/metacubex/mihomo/tunnel"
-	"net/netip"
-	"time"
 )
 
 type InitParams struct {
@@ -79,39 +80,41 @@ type ProxiesData struct {
 }
 
 const (
-	messageMethod                  CoreMethod = "message"
-	initClashMethod                CoreMethod = "initClash"
-	getIsInitMethod                CoreMethod = "getIsInit"
-	forceGcMethod                  CoreMethod = "forceGc"
-	shutdownMethod                 CoreMethod = "shutdown"
-	validateConfigMethod           CoreMethod = "validateConfig"
-	updateConfigMethod             CoreMethod = "updateConfig"
-	getProxiesMethod               CoreMethod = "getProxies"
-	changeProxyMethod              CoreMethod = "changeProxy"
-	getTrafficMethod               CoreMethod = "getTraffic"
-	getTotalTrafficMethod          CoreMethod = "getTotalTraffic"
-	resetTrafficMethod             CoreMethod = "resetTraffic"
-	asyncTestDelayMethod           CoreMethod = "asyncTestDelay"
-	getConnectionsMethod           CoreMethod = "getConnections"
-	closeConnectionsMethod         CoreMethod = "closeConnections"
-	resetConnectionsMethod         CoreMethod = "resetConnections"
-	closeConnectionMethod          CoreMethod = "closeConnection"
-	getExternalProvidersMethod     CoreMethod = "getExternalProviders"
-	getExternalProviderMethod      CoreMethod = "getExternalProvider"
-	getCountryCodeMethod           CoreMethod = "getCountryCode"
-	getMemoryMethod                CoreMethod = "getMemory"
-	updateGeoDataMethod            CoreMethod = "updateGeoData"
-	updateExternalProviderMethod   CoreMethod = "updateExternalProvider"
-	sideLoadExternalProviderMethod CoreMethod = "sideLoadExternalProvider"
-	startLogMethod                 CoreMethod = "startLog"
-	stopLogMethod                  CoreMethod = "stopLog"
-	startListenerMethod            CoreMethod = "startListener"
-	stopListenerMethod             CoreMethod = "stopListener"
-	updateDnsMethod                CoreMethod = "updateDns"
-	crashMethod                    CoreMethod = "crash"
-	setupConfigMethod              CoreMethod = "setupConfig"
-	getConfigMethod                CoreMethod = "getConfig"
-	deleteFileMethod               CoreMethod = "deleteFile"
+	messageMethod                        CoreMethod = "message"
+	initClashMethod                      CoreMethod = "initClash"
+	getIsInitMethod                      CoreMethod = "getIsInit"
+	forceGcMethod                        CoreMethod = "forceGc"
+	shutdownMethod                       CoreMethod = "shutdown"
+	validateConfigMethod                 CoreMethod = "validateConfig"
+	updateConfigMethod                   CoreMethod = "updateConfig"
+	getProxiesMethod                     CoreMethod = "getProxies"
+	changeProxyMethod                    CoreMethod = "changeProxy"
+	getTrafficMethod                     CoreMethod = "getTraffic"
+	getTotalTrafficMethod                CoreMethod = "getTotalTraffic"
+	resetTrafficMethod                   CoreMethod = "resetTraffic"
+	asyncTestDelayMethod                 CoreMethod = "asyncTestDelay"
+	getConnectionsMethod                 CoreMethod = "getConnections"
+	closeConnectionsMethod               CoreMethod = "closeConnections"
+	resetConnectionsMethod               CoreMethod = "resetConnections"
+	closeConnectionMethod                CoreMethod = "closeConnection"
+	getExternalProvidersMethod           CoreMethod = "getExternalProviders"
+	getExternalProviderMethod            CoreMethod = "getExternalProvider"
+	getCountryCodeMethod                 CoreMethod = "getCountryCode"
+	getMemoryMethod                      CoreMethod = "getMemory"
+	updateGeoDataMethod                  CoreMethod = "updateGeoData"
+	updateExternalProviderMethod         CoreMethod = "updateExternalProvider"
+	sideLoadExternalProviderMethod       CoreMethod = "sideLoadExternalProvider"
+	startLogMethod                       CoreMethod = "startLog"
+	stopLogMethod                        CoreMethod = "stopLog"
+	startListenerMethod                  CoreMethod = "startListener"
+	stopListenerMethod                   CoreMethod = "stopListener"
+	updateDnsMethod                      CoreMethod = "updateDns"
+	crashMethod                          CoreMethod = "crash"
+	setupConfigMethod                    CoreMethod = "setupConfig"
+	getConfigMethod                      CoreMethod = "getConfig"
+	deleteFileMethod                     CoreMethod = "deleteFile"
+	generateAgeKeyPairMethod             CoreMethod = "generateAgeKeyPair"
+	convertAgeSecretKeyToPublicKeyMethod CoreMethod = "convertAgeSecretKeyToPublicKey"
 )
 
 type CoreMethod string
chenx-dust/FlClash-Patched

Shortcuts support

lib/core/ios.dart
@@ -14,6 +14,7 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
   static CoreIOS? _instance;
 
   static const _appCoreMethods = {
+    ActionMethod.initClash,
     ActionMethod.getIsInit,
     ActionMethod.validateConfig,
     ActionMethod.getConfig,

DanSheng-bot/FlClash

lib/core/ios.dart
@@ -15,6 +15,7 @@ class CoreIOS extends CoreHandlerInterface with ServiceListener {
   static CoreIOS? _instance;
 
   static const _appCoreMethods = {
+    CoreMethod.initClash,
     CoreMethod.getIsInit,
     CoreMethod.validateConfig,
     CoreMethod.getConfig,
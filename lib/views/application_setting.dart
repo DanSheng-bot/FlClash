chenx-dust/FlClash-Patched

Add toggle for disable invalid https certificates block

lib/views/application_setting.dart
@@ -330,6 +330,32 @@ class AutoCheckUpdateItem extends ConsumerWidget {
   }
 }
 
+class IgnoreCertificateErrorsItem extends ConsumerWidget {
+  const IgnoreCertificateErrorsItem({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appLocalizations = context.appLocalizations;
+    final ignoreCertificateErrors = ref.watch(
+      appSettingProvider.select((state) => state.ignoreCertificateErrors),
+    );
+    return ListItem.switchItem(
+      title: Text(appLocalizations.ignoreCertificateErrors),
+      subtitle: Text(appLocalizations.ignoreCertificateErrorsDesc),
+      delegate: SwitchDelegate(
+        value: ignoreCertificateErrors,
+        onChanged: (bool value) {
+          ref
+              .read(appSettingProvider.notifier)
+              .update(
+                (state) => state.copyWith(ignoreCertificateErrors: value),
+              );
+        },
+      ),
+    );
+  }
+}
+
 class ForegroundTickerIntervalItem extends ConsumerWidget {
   const ForegroundTickerIntervalItem({super.key});
 
@@ -542,6 +568,7 @@ class ApplicationSettingView extends ConsumerWidget {
       const CloseConnectionsItem(),
       const UsageItem(),
       if (system.isAndroid) const NetworkSpeedNotificationItem(),
+      const IgnoreCertificateErrorsItem(),
       const AutoCheckUpdateItem(),
     ];
     return BaseScaffold(

DanSheng-bot/FlClash

lib/views/application_setting.dart
@@ -302,6 +302,30 @@ class AutoCheckUpdateItem extends ConsumerWidget {
   }
 }
 
+class IgnoreCertificateErrorsItem extends ConsumerWidget {
+  const IgnoreCertificateErrorsItem({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appLocalizations = context.appLocalizations;
+    final ignoreCertificateErrors = ref.watch(
+      appSettingProvider.select((state) => state.ignoreCertificateErrors),
+    );
+    return ListItem.toggle(
+      title: Text(appLocalizations.ignoreCertificateErrors),
+      subtitle: Text(appLocalizations.ignoreCertificateErrorsDesc),
+      value: ignoreCertificateErrors,
+      onChanged: (bool value) {
+        ref
+            .read(appSettingProvider.notifier)
+            .update(
+              (state) => state.copyWith(ignoreCertificateErrors: value),
+            );
+      },
+    );
+  }
+}
+
 class ForegroundTickerIntervalItem extends ConsumerWidget {
   const ForegroundTickerIntervalItem({super.key});
 
@@ -514,6 +538,7 @@ class ApplicationSettingView extends ConsumerWidget {
       const CloseConnectionsItem(),
       const UsageItem(),
       if (system.isAndroid) const NetworkSpeedNotificationItem(),
+      const IgnoreCertificateErrorsItem(),
       const AutoCheckUpdateItem(),
     ];
     return BaseScaffold(